45e04f4f2491865837f646b37c654c97e7b8f775
resyakKire <erikkayser843@gmail.com>
2021-08-27 14:22:15 -0300

    organização dos arquivos

--- AtividadeNotaFiscal/src/VolumesTransportados.java
+++
@@ -1,8 +0,0 @@
-public class VolumesTransportados {
-    private int quantidade;
-    private String especie;
-    private String marca;
-    private int numero;
-    private float pesoBruto;
-    private float pesoLiquido;
-}
